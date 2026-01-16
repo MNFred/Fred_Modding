ded321e70fe67a0e1622d7b10338fd055c8e7785
MNFred <maciejnawara208@gmail.com>
2026-01-16 18:42:10 +0100

    TH 1.3.1 update

--- TH34/Artifacts/duo/LockUp.cs
+++ TH34/Artifacts/duo/LockUp.cs
@@ -37,10 +37,6 @@ public class ArtifactLockUp : Artifact, ITH34Artifact
         });
         api.RegisterDuoArtifact(MethodBase.GetCurrentMethod()!.DeclaringType!, [ModEntry.Instance.TH34_Deck.Deck, TuckerApi.TuckerDeck]);
         ModEntry.Instance.Harmony.Patch(
-            original: AccessTools.DeclaredMethod(typeof(AMove), nameof(AMove.Begin)),
-            transpiler: new HarmonyMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(AMove_Begin_Transpiler))
-        );
-        ModEntry.Instance.Harmony.Patch(
 			original: AccessTools.DeclaredMethod(typeof(Card), nameof(Card.GetActionsOverridden)),
 			postfix: new HarmonyMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(Card_GetActionsOverridden_Postfix))
 		);
@@ -55,73 +51,14 @@ public class ArtifactLockUp : Artifact, ITH34Artifact
         {
             if(fromPlayer == true)
             {
-                return 1;
+                return 2;
             }else return 0;
         }else return 0;
     }
-    public static IEnumerable<CodeInstruction> AMove_Begin_Transpiler(IEnumerable<CodeInstruction> instructions, MethodBase method)
-    {
-        try
-        {
-            return new SequenceBlockMatcher<CodeInstruction>(instructions)
-                .Find([
-                    ILMatches.Ldarg(3),
-                    ILMatches.Ldfld("hand"),
-                    ILMatches.Call("OfType"),
-                    ILMatches.Call("Any"),
-                    ILMatches.Brfalse.GetBranchTarget(out var branchTarget)
-                ])
-                .Insert(SequenceMatcherPastBoundsDirection.After, SequenceMatcherInsertionResultingBounds.IncludingInsertion, [
-                    new CodeInstruction(OpCodes.Ldarg_2),
-                    new CodeInstruction(OpCodes.Ldarg_3),
-                    new CodeInstruction(OpCodes.Ldarg_0),
-                    new CodeInstruction(OpCodes.Call, AccessTools.DeclaredMethod(typeof(ArtifactLockUp), nameof(AMove_Begin_Transpiler_Sub))),
-                    new CodeInstruction(OpCodes.Ldarg_0),
-                    new CodeInstruction(OpCodes.Ldfld, AccessTools.DeclaredField(typeof(AMove), nameof(AMove.fromEvade))),
-                    new CodeInstruction(OpCodes.Brfalse, branchTarget.Value)
-                ])
-                .Find([
-                    ILMatches.Ldloc<Ship>(method),
-                    ILMatches.LdcI4((int)Status.lockdown),
-                    ILMatches.Call("Get"),
-                    ILMatches.LdcI4(0),
-                    ILMatches.Ble.GetBranchTarget(out branchTarget)
-                ])
-                .Insert(SequenceMatcherPastBoundsDirection.After, SequenceMatcherInsertionResultingBounds.IncludingInsertion, [
-                    new CodeInstruction(OpCodes.Ldarg_2),
-                    new CodeInstruction(OpCodes.Ldarg_3),
-                    new CodeInstruction(OpCodes.Ldarg_0),
-                    new CodeInstruction(OpCodes.Call, AccessTools.DeclaredMethod(typeof(ArtifactLockUp), nameof(AMove_Begin_Transpiler_Sub))),
-                    new CodeInstruction(OpCodes.Ldarg_0),
-                    new CodeInstruction(OpCodes.Ldfld, AccessTools.DeclaredField(typeof(AMove), nameof(AMove.fromEvade))),
-                    new CodeInstruction(OpCodes.Brfalse, branchTarget.Value)
-                ])
-                .Find([
-                    ILMatches.Ldloc<Ship>(method),
-                    ILMatches.LdcI4((int)Status.engineStall),
-                    ILMatches.Call("Get"),
-                    ILMatches.LdcI4(0),
-                    ILMatches.Ble.GetBranchTarget(out branchTarget)
-                ])
-                .Insert(SequenceMatcherPastBoundsDirection.After, SequenceMatcherInsertionResultingBounds.IncludingInsertion, [
-                    new CodeInstruction(OpCodes.Ldarg_2),
-                    new CodeInstruction(OpCodes.Ldarg_3),
-                    new CodeInstruction(OpCodes.Ldarg_0),
-                    new CodeInstruction(OpCodes.Call, AccessTools.DeclaredMethod(typeof(ArtifactLockUp), nameof(AMove_Begin_Transpiler_Sub))),
-                    new CodeInstruction(OpCodes.Ldarg_0),
-                    new CodeInstruction(OpCodes.Ldfld, AccessTools.DeclaredField(typeof(AMove), nameof(AMove.fromEvade))),
-                    new CodeInstruction(OpCodes.Brfalse, branchTarget.Value)
-                ])
-                .AllElements();
-        }
-        catch(Exception ex)
-        {
-            ModEntry.Instance.Logger!.LogError("Could not patch method {method} - {Mod} probably won't work. \nReason: {Exception}", method, "Hell is full", ex);
-            return instructions;
-        }
-    }
     public bool IsEvadeActionEnabled(IKokoroApi.IV2.IEvadeHookApi.IHook.IIsEvadeActionEnabledArgs args)
     {
+        if (args.State.EnumerateAllArtifacts().FirstOrDefault(a => a is ArtifactLockUp) is not { } artifact)
+			return true;
         if(args.State.ship.Get(ModEntry.Instance.MinusChargeStatus.Status)>0)
         {
             return false;
@@ -143,20 +80,4 @@ public class ArtifactLockUp : Artifact, ITH34Artifact
 			}
 		}
 	}
-    public static void AMove_Begin_Transpiler_Sub(State s, Combat c, AMove move)
-    {
-        var card = ModEntry.Instance.KokoroApi.V2.ActionInfo.GetSourceCard(s, move);
-        if(card == null)
-            return;
-        if(move.fromEvade == false)
-        {
-            if(s.ship.Get(ModEntry.Instance.MinusChargeStatus.Status)>0)
-            {
-                move.dir = 0;
-                Audio.Play(Event.Status_PowerDown);
-                s.ship.shake += 1;
-                if(s.ship.Get(Status.engineStall)>0) s.ship.Add(Status.engineStall, -1);
-            }
-        }
-    }
 }
